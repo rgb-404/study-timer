7588d0e83b6f0b28e207b1eb24a7a4b3af706b0e
raghav <[bhupatiraju.raghav@gmail.com]>
2025-09-11 00:15:19 +0800

    added code for opening/closing app's windows/ processes beyond terminal manual access

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-
-
 // app and BrowserWindow are two Electron modules. App controls each event lifecycle of this app and BrowserWindow manages this app's windows
+// this desktop application's instance is henceforth 'app' as you can see in code
 const { app, BrowserWindow } = require('electron')
 
 
@@ -17,6 +16,17 @@ const createWindow = () => {
 
 
 //calls createWindow() when the app is ready using more electron-native packages
+//create's a window if when activated, there are no windows
 app.whenReady().then(() => {
     createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow
+    })
 })
+
+
+//'darwin' is the apple code for if an application is running, this quits it without manually going to terminal
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+}) 
