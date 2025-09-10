cc6fd60c31e3f3b447c466495c7047ee6d9222e3
raghav <[bhupatiraju.raghav@gmail.com]>
2025-09-11 00:11:44 +0800

    added base code to create a desktop window

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("Hello from Electron");
+
+
+// app and BrowserWindow are two Electron modules. App controls each event lifecycle of this app and BrowserWindow manages this app's windows
+const { app, BrowserWindow } = require('electron')
+
+
+
+//createWindow() loads the webpage into a new BrowserWindow, under the name 'win'
+const createWindow = () => {
+    const win = new BrowserWindow ({
+        width: 800,
+        height: 600,
+    })
+    
+    win.loadFile('index.html')
+}
+
+
+//calls createWindow() when the app is ready using more electron-native packages
+app.whenReady().then(() => {
+    createWindow()
+})
